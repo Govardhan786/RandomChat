331638f2d52260dd3bbbf49952f3a3dcb08f2187
Govardhan786 <117737421+Govardhan786@users.noreply.github.com>
2024-10-18 22:36:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,36 +12,36 @@ let users = {};
 // Serve the static files
 app.use(express.static('public'));
 
-// Listen for connection
+// Listen for connections
 io.on('connection', (socket) => {
     console.log('New user connected');
 
     // User joins with a username
     socket.on('join', (username) => {
-        users[socket.id] = username;
+        users[socket.id] = username; // Store the username associated with the socket ID
         io.emit('userList', Object.values(users)); // Send updated user list to everyone
         socket.broadcast.emit('message', { username: 'System', text: `${username} joined the chat!` });
     });
 
     // Send group messages
     socket.on('sendMessage', (message) => {
-        io.emit('message', { username: users[socket.id], text: message });
+        io.emit('message', { username: users[socket.id], text: message }); // Broadcast message to all users
     });
 
     // Send private messages
     socket.on('privateMessage', (data) => {
         const recipientSocketId = Object.keys(users).find(key => users[key] === data.to);
         if (recipientSocketId) {
-            io.to(recipientSocketId).emit('privateMessage', { from: users[socket.id], text: data.message });
+            io.to(recipientSocketId).emit('privateMessage', { from: users[socket.id], text: data.message }); // Send private message to the recipient
         }
     });
 
     // Handle user disconnect
     socket.on('disconnect', () => {
         const username = users[socket.id];
-        delete users[socket.id];
-        io.emit('userList', Object.values(users)); // Send updated user list
-        socket.broadcast.emit('message', { username: 'System', text: `${username} left the chat!` });
+        delete users[socket.id]; // Remove user from the list
+        io.emit('userList', Object.values(users)); // Send updated user list to all users
+        socket.broadcast.emit('message', { username: 'System', text: `${username} left the chat!` }); // Notify others about the user leaving
     });
 });
 
